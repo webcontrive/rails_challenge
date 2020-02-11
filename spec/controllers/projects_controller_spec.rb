76ae3ec205a6d020fe00aad90f65be859ce7c85e
Nikund <wc.nikund@gmail.com>
2020-02-11 11:20:31 +0530

    Challenge 3: Rspec Unit Tests

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,18 +3,52 @@ require 'rails_helper'
 RSpec.describe ProjectsController, type: :controller do
   describe "GET index" do
     it "return value index" do
-      params = {user_id: 1}
-      projects = Project.joins(:project_users).where(project_users: {user_id: params[:user_id]}).all
-      if params[:sort] == 'name'
-        Project.all.order(params[:sort])
-      end
+      params = {user_id: 1, sort: 'name'}
+      sort = params[:sort] if params[:sort] == 'name'
+      projects = Project.left_outer_joins(:project_users).where("projects.manager_id = :userid or project_users.user_id = :userid", userid: params[:user_id]).order(sort).uniq
     end
   end
 
   describe "POST create" do
-    it "create record" do
+
+    before(:each) do
       params = {user_id: 1}
-      @user = Project.new(name: 'Test', manager_id: 1)
+      @user = User.find_by(id: params[:user_id])
+      if @user.blank?
+        puts "User are invalid"
+      end
+    end
+
+    context 'when user does not exist' do
+      context 'with invalid params' do
+        it 'invalid project name' do
+          unless @user.blank?
+            invalid_params_name = ''
+            project = Project.new(name: invalid_params_name, manager_id: @user.id)
+            unless project.save
+              puts project.errors.full_messages
+            end
+          end
+        end
+      end
+
+      context 'with valid params' do
+        let(:valid_params) do
+          {
+            project: {name: 'Test'}
+          }
+        end
+        it 'responds with success' do
+          unless @user.blank?
+            project = Project.new(name: valid_params[:project][:name], manager_id: @user.id)
+            if project.save
+              puts 'Record successfully save'
+            else
+              puts project.errors.full_messages
+            end
+          end
+        end
+      end
     end
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Project, type: :model do
   describe Project do
     it { should validate_presence_of :name }
     it { should validate_length_of(:name).is_at_least(2) }
-    it { should have_many(:users) }
+    it { should have_many(:project_users) }
+    it { should have_many(:users).through(:project_users) }
     it { should belong_to(:manager) }
   end
 end
